638cdf1ac459976cf7f149218e56651c02e1d2bb
Tushar Kale <tushar.K197@gmail.com>
2018-09-13 11:02:46 -0400

    Added the recursive implementation of mergesort and also finished the implementation of merge_insertion sort which picks a sorting algo based on input size

--- AOA/merge_insertion_sort.py
+++ AOA/merge_insertion_sort.py
@@ -1,10 +1,13 @@
 # Implement a combination of merge insertion sort and record the times
 import time
 import AOA.sorting_algos as sa
+import random
 
 
 def merge_insertion_sort(input_list, pivot, select_sort):
     """
+    Function to sort elements of a list based on a pivot.
+    For smaller size lists insertion sort is used. For list of length > pivot merge sort is used
 
     :param input_list: The list of numbers that needs sorting
     :param pivot:
@@ -12,35 +15,41 @@ def merge_insertion_sort(input_list, pivot, select_sort):
     :return: execution times of the mixed
     """
     start_time = time.time()
-    insr_time = 0
-    mrg_time = 0
     output_list = list()
     if select_sort == 0:
 
         if len(input_list) <= pivot:
             output_list = sa.insertion_sort(input_list)
-            print("Call insertion sort here")
         else:
-            print("Call merge sort here")
+            output_list = sa.merge_sort(input_list)
 
     elif select_sort == 1:
-        mrg_str_time = time.time()
-        print("use only merge sort")
-        mrg_end_time = time.time()
-        mrg_time = mrg_end_time - mrg_str_time
+
+        output_list = sa.merge_sort(input_list)
 
     elif select_sort == 2:
-        insr_str_time = time.time()
-        print("use only insertion sort")
-        insr_end_time = time.time()
-        insr_time = insr_end_time - insr_str_time
+        output_list = sa.insertion_sort(input_list)
 
     end_time = time.time()
     total_time = end_time-start_time
-    return output_list, total_time, insr_time, mrg_time
+    return output_list, total_time
 
 
 if __name__ == "__main__":
-    a = [5, 4, 3, 2, 1]
-    exec_time = merge_insertion_sort(a, 100, 0)
+    num_elems = 10000
+    my_randoms = [random.randrange(1, num_elems+1, 1) for _ in range(num_elems)]
 
+    op_list, exec_time = merge_insertion_sort(my_randoms, 100, 2)
+    print("Insertion sort time")
+    print(op_list)
+    print(exec_time)
+
+    op_list, exec_time = merge_insertion_sort(my_randoms, 100, 1)
+    print("Merge sort time")
+    print(op_list)
+    print(exec_time)
+
+    op_list, exec_time = merge_insertion_sort(my_randoms, 100, 0)
+    print("Merge Insertion sort time")
+    print(op_list)
+    print(exec_time)
--- AOA/sorting_algos.py
+++ AOA/sorting_algos.py
@@ -1,3 +1,4 @@
+import math
 def insertion_sort(input_list):
     """
         Implementation of Insertion Sort
@@ -18,6 +19,62 @@ def insertion_sort(input_list):
         raise exc
 
 
+def merge_sort(input_list):
+    """
+    Recursive implementaton of merge sort
+    :param input_list:
+    :param start:
+    :param end:
+    :return:
+    """
+    try:
+
+        if len(input_list) <= 1:
+            return input_list
+        else:
+            pivot = math.floor(len(input_list)/2)
+            first_half = merge_sort(input_list[:pivot])
+            second_half = merge_sort(input_list[pivot:])
+        return merge(first_half, second_half)
+    except Exception as exc:
+        raise exc
+
+
+def merge(list_1, list_2):
+    """
+    The MERGE step of merge sort
+    :param list_1:
+    :param list_2:
+    :return: merged sorted list -> a combination of list_1 and list_2
+    """
+    try:
+        i = 0
+        j = 0
+        m = 0
+        merged_list = list()
+
+        while i < len(list_1) and j < len(list_2):
+            if list_1[i] <= list_2[j]:
+                merged_list.append(list_1[i])
+                i = i+1
+            else:
+                merged_list.append(list_2[j])
+                j = j+1
+
+        while i < len(list_1):
+            merged_list.append(list_1[i])
+            i = i+1
+        while j < len(list_2):
+            merged_list.append(list_2[j])
+            j = j + 1
+        return merged_list
+    except Exception as exc:
+        raise exc
+
+
 if __name__ == "__main__":
-    a = [4, 3, 2, 1, -2, 6, 9, 10, 27278, 88, 5, 2, 4, 6, 1, 3]
-    print(insertion_sort(a))
+    l1 = [1, 3, 5, 7, 9]
+    l2 = [2, 4, 6, 8, 10]
+    l3 = [3, 6, 11, 2, 7, 0, 1, 44, 6, 3, 78]
+    print(merge(l1, l2))
+    print(merge_sort(l3))
