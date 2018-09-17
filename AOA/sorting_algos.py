b8b44160dd7853e2e1f8c214aa916d5a37afe671
Tushar Kale <tushar.K197@gmail.com>
2018-09-17 17:00:31 -0400

    Added Heapsort, quicksort, counting sort and other utils methods for both

--- AOA/sorting_algos.py
+++ AOA/sorting_algos.py
@@ -1,4 +1,7 @@
 import math
+import AOA.utils as algos_utils
+
+
 def insertion_sort(input_list):
     """
         Implementation of Insertion Sort
@@ -72,9 +75,75 @@ def merge(list_1, list_2):
         raise exc
 
 
+def heap_sort(input_list):
+    """
+
+    :param input_list: List that needs to be sorted
+    :return:
+    """
+    try:
+        input_list = algos_utils.build_max_heap(input_list)
+        for element in range(len(input_list)-1, -1, -1):
+            input_list[0], input_list[element] = input_list[element], input_list[0]
+            input_list = algos_utils.max_heapify(input_list, 0, element-1)
+        return input_list
+    except Exception as exc:
+        raise exc
+
+
+def quick_sort(input_list, start, end):
+    """
+        Recursive implementaion of quicksort
+    :param input_list:
+    :param start
+    :param end
+    :return:
+    """
+    try:
+        if start < end:
+            pivot, input_list = algos_utils.partition(input_list, start, end)
+            quick_sort(input_list, start, pivot-1)
+            quick_sort(input_list, pivot + 1, end)
+        return input_list
+    except Exception as exc:
+        raise exc
+
+
+def counting_sort(input_list, k):
+    """
+
+    :param input_list:
+    :param k:
+    :return:
+    """
+    try:
+        input_len = len(input_list)
+        count_list = [0] * (k+1)
+
+        for i in range(0, input_len):
+            count_list[input_list[i]] = count_list[input_list[i]] + 1
+        for j in range(1, k+1):
+            count_list[j] = count_list[j-1]+count_list[j]
+        output_list = [0] * input_len
+        for l in range(input_len-1, -1, -1):
+            # To account for the list index starting from 0
+            # had to subtract 1 in the next line(output_list[count_list[input_list[l]]-1] = input_list[l])
+            # to find the correct position of the said element
+            output_list[count_list[input_list[l]]-1] = input_list[l]
+            count_list[input_list[l]] = count_list[input_list[l]] - 1
+        return output_list
+
+    except Exception as exc:
+        raise exc
+
+
 if __name__ == "__main__":
     l1 = [1, 3, 5, 7, 9]
     l2 = [2, 4, 6, 8, 10]
     l3 = [3, 6, 11, 2, 7, 0, 1, 44, 6, 3, 78]
-    print(merge(l1, l2))
-    print(merge_sort(l3))
+    l4 = [2, 5, 3, 0, 2, 3, 0, 3]
+    # print(merge(l1, l2))
+    # print(merge_sort(l3))
+    # print(heap_sort(l3))
+    # print(quick_sort(l3, 0, len(l3)-1))
+    print(counting_sort(l4, 5))
--- AOA/utils.py
+++ AOA/utils.py
@@ -1,27 +1,32 @@
+import math
 
-def max_heapify(input_list, element_index):
+# **** HEAP OPERATIONS **** #
+
+
+def max_heapify(input_list, element_index, heap_size):
     """
         Function to max heapify a given heap for an element at element_index
     :param input_list:
     :param element_index:
+    :param heap_size
     :return:
     """
     try:
         left = left_child(element_index)
         right = right_child(element_index)
-        last_index = len(input_list) - 1
 
-        if left <= last_index and input_list[left] >= input_list[element_index]:
+        if left <= heap_size and input_list[left] >= input_list[element_index]:
             largest = left
         else:
             largest = element_index
 
-        if right <= last_index and input_list[right] >= input_list[element_index]:
+        if right <= heap_size and input_list[right] >= input_list[largest]:
             largest = right
 
         if largest != element_index:
             input_list[element_index], input_list[largest] = input_list[largest], input_list[element_index]
-            max_heapify(input_list, largest)
+            max_heapify(input_list, largest, heap_size)
+
         return input_list
     except Exception as exc:
         raise exc
@@ -49,7 +54,7 @@ def min_heapify(input_list, element_index):
 
         if smallest != element_index:
             input_list[element_index], input_list[smallest] = input_list[smallest], input_list[element_index]
-            max_heapify(input_list, smallest)
+            min_heapify(input_list, smallest)
 
         return input_list
     except Exception as exc:
@@ -76,6 +81,18 @@ def right_child(element_index):
     return (2*element_index) + 2
 
 
+def build_max_heap(input_list):
+    """
+    Given a list this function turns it into a max heap
+    :param input_list:
+    :return:
+    """
+    for i in range(math.floor(len(input_list)/2), -1, -1):
+        input_list = max_heapify(input_list, i, len(input_list)-1)
+
+    return input_list
+
+
 # def max_heapify_iter(input_list, element_index):
 #     """
 # ******* PSUEDOCODE WRITTEN IN THE BOOK. IMPLEMENT IT LATER *******
@@ -89,6 +106,64 @@ def right_child(element_index):
 #     while j <=
 # except Exception as exc:
 #     raise exc
+
+# **** Quicksort utils****
+
+def partition(input_list, start, end):
+    """
+
+    :param input_list: List that needs to be partioned around a pivot
+    :param start: Start index of the list
+    :param end: End index of the list
+    :return: returns the index of the pivot
+    """
+    try:
+        start_pointer = start
+        end_pointer = end-1
+
+        # FOLLOWING logic works just fine! Just that the alternate solution is a less complicated implementation of the same strategy
+        # while start_pointer < end_pointer:
+        #     if input_list[start_pointer] > input_list[end] and input_list[end_pointer] <= input_list[end]:
+        #         input_list[start_pointer], input_list[end_pointer] = input_list[end_pointer], input_list[start_pointer]
+        #         start_pointer = start_pointer + 1
+        #         end_pointer = end_pointer - 1
+        #
+        #     if input_list[start_pointer] <= input_list[end] and input_list[end_pointer] > input_list[end]:
+        #         start_pointer = start_pointer + 1
+        #         end_pointer = end_pointer - 1
+        #
+        #     if input_list[start_pointer] <= input_list[end] and input_list[end_pointer] <= input_list[end]:
+        #         start_pointer = start_pointer + 1
+        #
+        #     if input_list[start_pointer] > input_list[end] and input_list[end_pointer] > input_list[end]:
+        #         end_pointer = end_pointer - 1
+        # if input_list[start_pointer] <= input_list[end]:
+        #     input_list[start_pointer+1], input_list[end] = input_list[end], input_list[start_pointer+1]
+        #     pivot_index = start_pointer + 1
+        # else:
+        #     input_list[start_pointer], input_list[end] = input_list[end], input_list[start_pointer]
+        #     pivot_index = start_pointer
+        while start_pointer <= end_pointer:
+            while input_list[start_pointer] < input_list[end]:
+                start_pointer = start_pointer + 1
+
+            while input_list[end_pointer] > input_list[end]:
+                end_pointer = end_pointer - 1
+
+            if start_pointer <= end_pointer:
+                input_list[start_pointer], input_list[end_pointer] = input_list[end_pointer], input_list[start_pointer]
+                start_pointer = start_pointer + 1
+                end_pointer = end_pointer - 1
+
+        pivot_index = start_pointer
+        input_list[pivot_index], input_list[end] = input_list[end], input_list[pivot_index]
+        return pivot_index, input_list
+
+    except Exception as exc:
+        raise exc
+
+
 if __name__ == "__main__":
-    a = [27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0]
-    print(max_heapify(a, 2))
+    b = [15, 3, 2, 1, 9, 5, 7, 8, 6]
+    # print(max_heapify(a, 1, len(a)-1))
+    print(partition(b, 0, len(b)-1))
