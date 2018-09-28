e0b590febeee9f7ebc921f32fa0cf4f299ff6768
tusharkaley <tomcat1903>
2018-09-27 23:57:39 -0400

    Empty python file for classifier

---
+++ MIS/HW4/multiclass_linear_classifier.py
@@ -0,0 +1,15 @@
+"""
+Math for Intelligent Systems Homework 4
+Problem statement:
+2. Multi-class Linear Classifier
+•   Download the Iris dataset available at the UCI Machine Learning repository in the platform of your choice.
+•   Execute a homegrown multi-class least-squares classifier on the Iris dataset after separating it into training and test set patterns.
+•   Documentthetrainingandtestingmis-classificationerrorsandseparateouteachclasserrors using a confusion matrix (3 × 3 with diagonal values being true positives).
+•   Document the settings of the free parameters (if any). Give a high level summary of your findings based on your interpretation of the results.
+•   The ratios attempted for training and testing should be 12% training/88% testing, 30% train- ing/70% testing and 50% training/50% testing
+    (and these percentages should be class spe- cific as much as possible). There is no need to perform cross-validation.
+•   Include the source code and printouts in your submitted homework. Add a link to the pdf if you wish.
+"""
+
+if __name__ == "__main__":
+    print("All the HW 4 classifier code goes here")
